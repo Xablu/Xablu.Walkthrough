c18605e2ac2a5967b550532baae1558d15bb3e4e
Rick Neeft <rick@chroomsoft.nl>
2017-06-08 13:57:35 +0200

    Added a factory class for the cross walkthrough

---
+++ src/Xablu.Walkthrough/Plugin.Xablu.Walkthrough/CrossWalkthoughFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using Plugin.Xablu.Walkthrough.Abstractions;
+
+namespace Plugin.Xablu.Walkthrough
+{
+    public class CrossWalkthoughFactory : IWalkthroughFactory
+    {
+        private readonly Lazy<IWalkthrough> _walkthrough;
+
+        public CrossWalkthoughFactory()
+        {
+            _walkthrough = new Lazy<IWalkthrough>(() => CreateWalkthrough(), LazyThreadSafetyMode.PublicationOnly);
+        }
+
+        public IWalkthrough Create()
+        {
+            if (!_walkthrough.IsValueCreated)
+                throw CrossWalkthrough.NotImplementedInReferenceAssembly();
+
+            return _walkthrough.Value;
+        }
+
+        private IWalkthrough CreateWalkthrough()
+        {
+#if PORTABLE
+            return null;
+#else
+            return new WalkthroughImplementation();
+#endif
+        }
+    }
+}
---
+++ src/Xablu.Walkthrough/Plugin.Xablu.Walkthrough/IWalkthroughFactory.cs
@@ -0,0 +1,9 @@
+﻿using Plugin.Xablu.Walkthrough.Abstractions;
+
+namespace Plugin.Xablu.Walkthrough
+{
+    public interface IWalkthroughFactory
+    {
+        IWalkthrough Create();
+    }
+}
